614dc111f57778a26502daf81f1dbba2bb0538e4
Esyeha <syh48149@gmail.com>
2024-03-14 11:17:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import {getMovieList, searchMovie} from './api'
 
-function App() {
+ const App = () => {
+  const [popularMovie ,setPopularMovie] = useState([])
+  const [isNotif, setIsNotif] = useState(false)
+
+  useEffect(() => {
+      getMovieList().then((results) => {
+      setPopularMovie(results)
+    })
+  },[])
+
+  const notif = () => {
+    setIsNotif(!isNotif)
+    // setTimeout(() => {
+    //   setIsNotif((state) => !state )
+    // }, 2000)
+  }
+
+  // console.log({ isNotif })
+
+  const PopularMovieList = () => {
+    return popularMovie.map((movie, i) => {
+      return (
+          <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img src={`${process.env.REACT_APP_BASEIMGURL}${movie.poster_path}`} alt="" className="Movie-img" />
+            <div className="Movie-date">release : {movie.release_date}</div>
+            <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      )
+    })
+  }
+
+  const search = async (q) => {
+    if(q.length > 3) {
+      const query = await searchMovie(q)
+      setPopularMovie(query.results)
+    }
+  }
+
   return (
     <div className="App">
+      {isNotif && <h3>puhehehehehe</h3>}
+      <button onClick={notif} className='btn'>
+        {isNotif ? "notifikasi OFF" : "notifikasi ON"}
+      </button>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Esyeha Movie DB</h1>
+        <input
+         placeholder='Cari Film Kesayangan...' 
+         className='Movie-search'
+         onChange={({target}) => search(target.value)}
+         />
+          <div className="Movie-container">
+          <PopularMovieList/>
+        </div>
       </header>
     </div>
-  );
-}
+  )
+  }
 
-export default App;
+export default App
